e627b94270f4f543fd1f6cb23dafb085f2b80ead
dhindsa-03 <dhindsagurtaj@gmail.com>
2022-03-26 22:44:13 -0400

    UPDATE GD 10:44PM - (3/26/2022)

--- app/src/main/java/com/example/digitdilema/GameScreen.java
+++ app/src/main/java/com/example/digitdilema/GameScreen.java
@@ -1,77 +1,89 @@
 package com.example.digitdilema;
-
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.Gravity;
-import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.MotionEvent;
+
 import android.view.View;
 import android.widget.EditText;
-import android.widget.LinearLayout;
-import android.widget.PopupWindow;
+
 import android.widget.TextView;
 
-import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.constraintlayout.widget.ConstraintLayout;
 
-import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class GameScreen extends AppCompatActivity {
+    int max = getMax();
+    int l;
+
+
+    public int getMax(){
+        if(x.getLevel()==1){
+            l = 0;
+            return 20;
+        }else if(x.getLevel()==2){
+            l = 1;
+            return 50;
+        }else{
+            l=2;
+            return 100;
+        }
+    }
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.guessingscreen);
+        TextView level = (TextView) findViewById(R.id.displayLevel);
+        if(l == 0){
+            level.setText("Level: Easy");
+        }else if(l == 1){
+            level.setText("Level: Medium");
+        }else if(l == 2){
+            level.setText("Level: Hard");
+        }
     }
+
     private static ScoreBoard x = InputNameActivity.getCurrent();
     int min = 1;
-    int max = getMax();
-    int result = getRandom(min, max);
+    int result = getRandom(min,max);
+
     private int score;
     private Player player = x.getPlayer();
-
     public static int getRandom(int min, int max){
         return (int) ((Math.random() * (max - min)) + min);
     }
-    public int getMax(){
-        if(x.getLevel()==1){
-            return 20;
-        }else if(x.getLevel()==2){
-            return 50;
-        }else{
-            return 100;
-        }
-    }
+
+
+
     public void buttonClicked(View v) {
-        //
+
         int guess;
         //
         EditText guessInt = (EditText) findViewById(R.id.guessInt);
         guess = Integer.parseInt(guessInt.getText().toString());
         score++;
         //
-        if (guess < result){
-            ((TextView) findViewById(R.id.answer)).setText(max+"Too Low");
-        } else if (guess > result){
-            ((TextView) findViewById(R.id.answer)).setText(max+ "Too High");
+        if (guess < result) {
+            ((TextView) findViewById(R.id.answer)).setText("Your Guess Is Too Low");
+        } else if (guess > result) {
+            ((TextView) findViewById(R.id.answer)).setText("Your Guess Is Too High");
         } else {
-            if(x.checkName()!=-1){
-                if(player.getScore()<score){
+            if (x.checkName() != -1) {
+                if (player.getScore() < score) {
                     ((TextView) findViewById(R.id.answer)).setText("Correct Guess!\n");
                     player.setScore(score);
-                }else{
+                } else {
                     ((TextView) findViewById(R.id.answer)).setText("Correct Guess!\n");
                 }
-            }else{
+            } else {
                 player.setScore(score);
             }
         }
-
+    }
+    public void resetGame(View v){
+        Intent intent = new Intent(getApplicationContext(), GameScreen.class);
+        startActivity(intent);
+        overridePendingTransition(0,0);
 
     }
-
-
 }
