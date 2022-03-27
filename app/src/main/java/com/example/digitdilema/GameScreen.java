34a7935694a7e5698fea01af4f739728809f9f9c
dhindsa-03 <dhindsagurtaj@gmail.com>
2022-03-26 21:20:04 -0400

    UPDATE GD 9:19PM - (3/26/2022)

--- app/src/main/java/com/example/digitdilema/GameScreen.java
+++ app/src/main/java/com/example/digitdilema/GameScreen.java
@@ -20,15 +20,42 @@ import androidx.constraintlayout.widget.ConstraintLayout;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+import org.w3c.dom.Text;
+
 public class GameScreen extends AppCompatActivity {
+    int max = getMax();
+    int l;
+
+
+    public int getMax(){
+        if(x.getLevel()==1){
+            l = 0;
+            return 20;
+        }else if(x.getLevel()==2){
+            l = 1;
+            return 50;
+        }else{
+            l=2;
+            return 100;
+        }
+    }
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.guessingscreen);
+        TextView level = (TextView) findViewById(R.id.displayLevel);
+        if(l == 0){
+            level.setText("Level: Easy");
+        }else if(l == 1){
+            level.setText("Level: Medium");
+        }else if(l == 2){
+            level.setText("Level: Hard");
+        }
     }
+
     private static ScoreBoard x = InputNameActivity.getCurrent();
     int min = 1;
-    int max = getMax();
     int result = 2;
 
             //getRandom(min, max);
@@ -38,25 +65,10 @@ public class GameScreen extends AppCompatActivity {
     public static int getRandom(int min, int max){
         return (int) ((Math.random() * (max - min)) + min);
     }
-    public int getMax(){
-        if(x.getLevel()==1){
-            return 20;
-        }else if(x.getLevel()==2){
-            return 50;
-        }else{
-            return 100;
-        }
-    }
+
 
     public void buttonClicked(View v) {
-        //
-        if(x.getLevel()==1){
-            ((TextView) findViewById(R.id.displayLevel)).setText("Level: Easy");
-        }else if(x.getLevel()==2){
-            ((TextView) findViewById(R.id.displayLevel)).setText("Level: Medium");
-        }else{
-            ((TextView) findViewById(R.id.displayLevel)).setText("Level: Hard");
-        }
+
         int guess;
         //
         EditText guessInt = (EditText) findViewById(R.id.guessInt);
@@ -64,9 +76,9 @@ public class GameScreen extends AppCompatActivity {
         score++;
         //
         if (guess < result) {
-            ((TextView) findViewById(R.id.answer)).setText(max + "Too Low");
+            ((TextView) findViewById(R.id.answer)).setText("Too Low");
         } else if (guess > result) {
-            ((TextView) findViewById(R.id.answer)).setText(max + "Too High");
+            ((TextView) findViewById(R.id.answer)).setText("Too High");
         } else {
             if (x.checkName() != -1) {
                 if (player.getScore() < score) {
